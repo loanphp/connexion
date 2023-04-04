1362c49c02675c2f3db87fee9bf8b723ac4079e7
unknown <loanlendamba@gmail.com>
2023-04-04 13:29:10 +0200

    initial commit

--- boulingui.sql
+++ boulingui.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 03 avr. 2023 à 17:24
+-- Généré le : mar. 04 avr. 2023 à 13:23
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.2.0
 
@@ -28,12 +28,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `eleve` (
-  `id_eleve` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `date_de_naissance` date NOT NULL DEFAULT current_timestamp(),
-  `lieu_naissance` varchar(255) NOT NULL
+  `nom_matiere` varchar(100) NOT NULL,
+  `note_eleve` int(2) NOT NULL,
+  `nom_eleve` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `eleve`
+--
+
+INSERT INTO `eleve` (`nom_matiere`, `note_eleve`, `nom_eleve`) VALUES
+('Espagnol', 17, 'loan'),
+('EPS', 18, 'LOLI'),
+('SVT', 16, 'POLO');
 
 -- --------------------------------------------------------
 
@@ -54,27 +61,10 @@ INSERT INTO `matiere` (`nom_matiere`, `coefficient`) VALUES
 ('Mathematique', 4),
 ('Francais', 3),
 ('EPS', 2),
-('EPS', 2);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `note_eleve`
---
-
-CREATE TABLE `note_eleve` (
-  `id_note_eleve` int(11) NOT NULL,
-  `Mathematique` int(2) NOT NULL,
-  `Francais` int(2) NOT NULL,
-  `EPS` int(2) NOT NULL,
-  `HG` int(2) NOT NULL,
-  `Espagnol` int(2) NOT NULL,
-  `Anglais` int(2) NOT NULL,
-  `Philosophie` int(2) NOT NULL,
-  `SVT` int(2) NOT NULL,
-  `Physique_Chimie` int(2) NOT NULL,
-  `Bureautique` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+('EPS', 2),
+('Mathematique', 4),
+('SVT', 3),
+('Bureautique', 2);
 
 -- --------------------------------------------------------
 
@@ -102,33 +92,7 @@ INSERT INTO `user` (`Email`, `Password`) VALUES
 -- Index pour la table `eleve`
 --
 ALTER TABLE `eleve`
-  ADD PRIMARY KEY (`id_eleve`);
-
---
--- Index pour la table `note_eleve`
---
-ALTER TABLE `note_eleve`
-  ADD KEY `id` (`id_note_eleve`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `eleve`
---
-ALTER TABLE `eleve`
-  MODIFY `id_eleve` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `note_eleve`
---
-ALTER TABLE `note_eleve`
-  ADD CONSTRAINT `note_eleve_ibfk_1` FOREIGN KEY (`id_note_eleve`) REFERENCES `eleve` (`id_eleve`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD PRIMARY KEY (`nom_eleve`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
